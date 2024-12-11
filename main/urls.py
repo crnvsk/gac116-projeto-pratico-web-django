1bdebbc171955d7ccbbfbb8e5606c6f825d52625
João Pedro Carneiro <107374203+crnvsk@users.noreply.github.com>
2024-12-10 23:17:42 -0300

    init: init repo
    Signed-off-by: João Pedro Carneiro <107374203+crnvsk@users.noreply.github.com>

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.http import HttpResponse
+
+def hello_world(request):
+    return HttpResponse("Hello, World!")
+
+urlpatterns = [
+    path('', hello_world, name='hello_world'),
+]
